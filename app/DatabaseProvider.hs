431be5293624eb9aabe297855d0238a74f929b9f
yhtq <1414672068@qq.com>
2025-11-17 19:18:00 +0800

    update

--- app/DatabaseProvider.hs
+++ app/DatabaseProvider.hs
@@ -1,9 +1,14 @@
 module DatabaseProvider where
 
+import Control.Exception.Base(try, catch, SomeException, ErrorCall)
+import Data.Effect.Except (Throw, throw)
 import Control.Monad.Hefty (
     raise,
+    AlgHandler,
+    Throw(..),
     FOEs,
     reinterpret,
+    reinterpretWith,
     Eff,
     Effect,
     Emb,
@@ -27,33 +32,69 @@ import Control.Monad.Hefty (
  )
 import Database.MongoDB (Pipe, Database, Action, master, access, connect, close, Host)
 import Control.Monad.Hefty.Reader (runAsk, ask)
+import Data.Text (Text, pack)
+import Control.Monad.Hefty.Except (Catch, runCatch)
+import qualified Control.Monad.Hefty.Except as Except
 
 
 newtype DataBaseHandler = DataBaseHandler Pipe
 newtype DataBaseTableHandler = DataBaseTableHandler (Pipe, Database)
 type DataBaseState = Ask DataBaseHandler
 type DataBaseTableState = Ask DataBaseTableHandler
+type ErrMessage = Text
+
+runThrow :: (FOEs es) => Eff (Throw e ': es) a -> Eff es (Either e a)
+runThrow = interpretBy (pure . Right) handleThrow
+
+runThrowPrint :: (Emb IO :> es, FOEs es, Show e) => Eff (Throw e ': es) a -> Eff es (Maybe a)
+runThrowPrint f = do
+    result <- runThrow f
+    case result of
+        Right val -> pure (Just val)
+        Left err -> do
+            liftIO $ putStrLn ("Error: " ++ show err)
+            pure Nothing
+
+handleThrow :: Applicative g => AlgHandler (Throw e) f g (Either e a)
+handleThrow (Throw e) _ = pure $ Left e
 
 data DatabaseIO :: Effect where
     RunDbAction :: forall a f. Action IO a -> DatabaseIO f a
 makeEffectF ''DatabaseIO
 
-runInDatabase :: forall es. (Emb IO :> es, FOEs es) => Host -> (Eff (DataBaseState ': es) ~> Eff es)
-runInDatabase host = interpretWith (
-        \Ask f -> do
-            pipe <- liftIO $ connect host
-            result <- f (DataBaseHandler pipe)
-            liftIO $ close pipe
-            pure result
-    )
+-- wrap all IO exceptions into ErrMessage
+safeLiftIO :: forall es a. (Emb IO :> es, Throw ErrMessage :> es) => IO a -> Eff es a
+safeLiftIO c = do
+    io_result <- liftIO $ do
+        catch (Right <$> c)
+            (\(e :: SomeException) -> return $ Left (pack $ show e))
+    case io_result of
+        Right val -> pure val
+        Left err -> throw err
+
+runInDatabase :: forall es. (Emb IO :> es, Throw ErrMessage :> es, FOEs es) => Host -> (Eff (DataBaseState ': es) ~> Eff (Catch ErrMessage ': es))
+runInDatabase host = reinterpretWith __handler 
+        where __handler :: AlgHandler DataBaseState f (Eff (Catch ErrMessage ': es)) a 
+              __handler Ask f = do
+                pipe <- safeLiftIO $ connect host
+                safeLiftIO $ putStrLn "Database connected."
+                result <- Except.catch 
+                    (Right <$> f (DataBaseHandler pipe))
+                    (\(e :: ErrMessage) -> pure $ Left e)
+                safeLiftIO $ putStrLn "Database connection closed."
+                safeLiftIO $ close pipe
+                case result of
+                    Right val -> pure val
+                    Left err -> throw err
+        
 runInDatabaseTable :: forall es. Database -> (Eff (DataBaseTableState ': es) ~> Eff (DataBaseState ': es))
 runInDatabaseTable db = reinterpret \case
     Ask -> do
         (DataBaseHandler pipe) <- ask
         pure $ DataBaseTableHandler (pipe, db)
 
-runDatabaseIO :: forall es. (Emb IO :> es, DataBaseTableState :> es) => Eff (DatabaseIO ': es) ~> Eff es
+runDatabaseIO :: forall es. (Emb IO :> es, Throw ErrMessage :> es, DataBaseTableState :> es) => Eff (DatabaseIO ': es) ~> Eff es
 runDatabaseIO = interpret \case
     RunDbAction action -> do
         (DataBaseTableHandler (pipe, db)) <- ask
-        liftIO $ access pipe master db action
+        safeLiftIO $ access pipe master db action
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DerivingVia #-}
 module Main (main) where
 
-import Prelude hiding (show, putStrLn, print)
+import Prelude hiding (show, putStrLn)
 import Data.Csv
 import GHC.Generics (Generic)
 import Data.Text (Text, show, unpack)
@@ -9,51 +9,90 @@ import Data.Vector qualified as V
 import Data.Text.IO (putStrLn)
 import Data.Text.IO.Utf8 qualified as TIO
 import CsvProvider
-import Control.Monad.Hefty (runEff, liftIO)
+import DatabaseProvider
+import Control.Monad.Hefty (runEff, liftIO, raise, runPure)
+import Database.MongoDB 
+import Control.Monad.Hefty.Except (throw, runCatch)
+import Control.Monad(void)
 
-video_info_data :: FilePath
-video_point_data :: FilePath
-video_info_data  = "/home/yhtq/data/cvse_video_info_data.csv"
-video_point_data = "/home/yhtq/data/cvse_video_point_data.csv"
 
--- A text type show as Unicode string
-newtype TextUnicode = TextUnicode Text deriving (Eq, FromField)
 
-instance Show TextUnicode where
-    showsPrec :: Int -> TextUnicode -> ShowS
-    showsPrec _ (TextUnicode t) = showString (unpack t)
+-- video_info_data :: FilePath
+-- video_point_data :: FilePath
+-- video_info_data  = "/home/yhtq/data/cvse_video_info_data.csv"
+-- video_point_data = "/home/yhtq/data/cvse_video_point_data.csv"
 
-data VideoInfo = VideoInfo
-    { avid      :: !TextUnicode
-    , duration      :: !Int
-    , desc  :: !TextUnicode
-    , pubdate  :: !TextUnicode
-    , bvid  :: !TextUnicode
-    , upid  :: !(Maybe Int)
-    , video_tags  :: !TextUnicode
-    , pubdate_unix    :: !Int
-    , upname :: !TextUnicode
-    , picture :: !TextUnicode
-    } deriving (Show, Generic)
+-- -- A text type show as Unicode string
+-- newtype TextUnicode = TextUnicode Text deriving (Eq, FromField)
 
--- instance Show VideoInfo where
---     showsPrec p vi = showParen (p > 10) $
---         showString "VideoInfo { avid = " . showsPrec 11 (TextUnicode $ avid vi) .
---         showString ", duration = " . showsPrec 11 (duration vi) .
---         showString ", desc = " . showsPrec 11 (TextUnicode $ desc vi) .
---         showString ", pubdate = " . showsPrec 11 (TextUnicode $ pubdate vi) .
---         showString ", bvid = " . showsPrec 11 (TextUnicode $ bvid vi) .
---         showString ", upid = " . showsPrec 11 (upid vi) .
---         showString ", video_tags = " . showsPrec 11 (TextUnicode $ video_tags vi) .
---         showString ", pubdate_unix = " . showsPrec 11 (pubdate_unix vi) .
---         showString ", upname = " . showsPrec 11 (TextUnicode $ upname vi) .
---         showString ", picture = " . showsPrec 11 (TextUnicode $ picture vi) .
---         showString " }"
+-- instance Show TextUnicode where
+--     showsPrec :: Int -> TextUnicode -> ShowS
+--     showsPrec _ (TextUnicode t) = showString (unpack t)
 
-instance FromNamedRecord VideoInfo
+-- data VideoInfo = VideoInfo
+--     { avid      :: !TextUnicode
+--     , duration      :: !Int
+--     , desc  :: !TextUnicode
+--     , pubdate  :: !TextUnicode
+--     , bvid  :: !TextUnicode
+--     , upid  :: !(Maybe Int)
+--     , video_tags  :: !TextUnicode
+--     , pubdate_unix    :: !Int
+--     , upname :: !TextUnicode
+--     , picture :: !TextUnicode
+--     } deriving (Show, Generic)
+
+-- -- instance Show VideoInfo where
+-- --     showsPrec p vi = showParen (p > 10) $
+-- --         showString "VideoInfo { avid = " . showsPrec 11 (TextUnicode $ avid vi) .
+-- --         showString ", duration = " . showsPrec 11 (duration vi) .
+-- --         showString ", desc = " . showsPrec 11 (TextUnicode $ desc vi) .
+-- --         showString ", pubdate = " . showsPrec 11 (TextUnicode $ pubdate vi) .
+-- --         showString ", bvid = " . showsPrec 11 (TextUnicode $ bvid vi) .
+-- --         showString ", upid = " . showsPrec 11 (upid vi) .
+-- --         showString ", video_tags = " . showsPrec 11 (TextUnicode $ video_tags vi) .
+-- --         showString ", pubdate_unix = " . showsPrec 11 (pubdate_unix vi) .
+-- --         showString ", upname = " . showsPrec 11 (TextUnicode $ upname vi) .
+-- --         showString ", picture = " . showsPrec 11 (TextUnicode $ picture vi) .
+-- --         showString " }"
+
+-- instance FromNamedRecord VideoInfo
+
+
+run :: Action IO ()
+run = do
+   clearTeams
+   insertTeams
+   allTeams >>= printDocs "All Teams"
+   nationalLeagueTeams >>= printDocs "National League Teams"
+   newYorkTeams >>= printDocs "New York Teams"
+   clearTeams
+
+clearTeams :: Action IO ()
+clearTeams = delete (select [] "team")
+
+insertTeams :: Action IO [Value]
+insertTeams = insertMany "team" [
+   ["name" =: "Yankees", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "American"],
+   ["name" =: "Mets", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "National"],
+   ["name" =: "Phillies", "home" =: ["city" =: "Philadelphia", "state" =: "PA"], "league" =: "National"],
+   ["name" =: "Red Sox", "home" =: ["city" =: "Boston", "state" =: "MA"], "league" =: "American"] ]
+
+allTeams :: Action IO [Document]
+allTeams = rest =<< find (select [] "team") {sort = ["home.city" =: 1]}
+
+nationalLeagueTeams :: Action IO [Document]
+nationalLeagueTeams = rest =<< find (select ["league" =: "National"] "team")
+
+newYorkTeams :: Action IO [Document]
+newYorkTeams = rest =<< find (select ["home.state" =: "NY"] "team") {project = ["name" =: 1, "league" =: 1]}
+
+printDocs :: Text -> [Document] -> Action IO ()
+printDocs title docs = liftIO $ putStrLn title >> mapM_ (print . exclude ["_id"]) docs
 
 main :: IO ()
-main = runEff . csvIOToIO $ do
-    video_info_datas <- readCsv @VideoInfo video_info_data
-    liftIO $ putStrLn $ "CSV data read successfully with length: " <> show (V.length video_info_datas)
-    liftIO $ putStrLn $ show (V.take 5 video_info_datas)
+main = runEff . void . runThrowPrint . runCatch  . (runInDatabase (host "127.0.0.1")) . (runInDatabaseTable "cvse_db")  . runDatabaseIO $ do
+    liftIO $ putStrLn "Database setup complete."
+    runDbAction run
+    throw "Test error message."
+    liftIO $ putStrLn "Deleted all documents from 'team' collection."
